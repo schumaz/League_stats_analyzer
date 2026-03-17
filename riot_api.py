df250bf822411feec875cd8fd65037caaf8647ac
schumaz <schumacher.goncalves@gmail.com>
2026-03-17 03:25:05 -0300

    feat: fetch user match history

--- riot_api.py
+++ riot_api.py
@@ -3,13 +3,20 @@ from config import api_key
 
 lol_watcher = LolWatcher(api_key)
 riot_watcher = RiotWatcher(api_key)
+region = "americas"
 
 def fetch_puuid(player_name, player_tag):
-    region = "americas"
-
     account = riot_watcher.account.by_riot_id(region, player_name, player_tag)
 
     return account["puuid"]
 
+def fecth_match_history(puuid):
+    match_ids = lol_watcher.match.matchlist_by_puuid(region, puuid)
+
+    return match_ids
+
 my_puuid = fetch_puuid("schumaZ", "fox")
-print(f"My puuid is: {my_puuid}")
+print(f"1. My puuid is: {my_puuid}")
+
+my_matches = fecth_match_history(my_puuid)
+print(f"2. My last matches is: {my_matches}")
